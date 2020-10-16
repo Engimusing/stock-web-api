c3a2dfd9adcce8ced4a1299a432065b5929ef10d
Megan <megan.miller@smoldev.dev>
2020-10-16 16:09:39 -0500

    database setup

--- setup/index.js
+++ setup/index.js
@@ -5,5 +5,5 @@ const cors = require("cors")
 module.exports = server => {
     server.use(helmet());
     server.use(cors());
-    server.use(express.json());
+    server.use(express.json({limit:"1000kb"}));
 };
